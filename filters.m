6de500ec268e0aba6ae85299f9258d4617534b0c
Jerry Tian <qitian41322@gmail.com>
2022-11-26 23:33:58 +0000

    high order filter

--- filters.m
+++ filters.m
@@ -1,19 +1,58 @@
 format short 
 format compact
 
-% Passive filters
-r1 = 31.4;
-L1 = 1e-3;
+% % Passive filters - low pass
+% r1 = 21.48;
+% L1 = 1e-3;
+% 
+% freq_vec = 1e2:1:1e6;
+% w = freq_vec * 2 * pi;
+% wc = r1/L1;
+% 
+% gain = 20*log10(wc ./ (j*w + wc));
+% 
+% figure(1); 
+% semilogx(freq_vec, gain);
+% text(wc/(2*pi), -3, "\leftarrow cut off frequency");
+% xlabel('frequency (Hz)');
+% ylabel('gain (dB)');
+% title('Bode Plot of an RL low pass filter')
 
-freq_vec = 1e3:1:1e5;
+% % Passive filters - band pass
+% r1 = 5e3;
+% L1 = 50e-3;
+% c1 = 50e-9;
+% 
+% freq_vec = 1e2:1:1e6;
+% w = freq_vec * 2 * pi;
+% wc = 20e3;
+% 
+% gain = 20*log10( j*w*L1 ./ (r1*(1-w.*w*L1*c1) + j*w*L1));
+% 
+% figure(1); 
+% semilogx(freq_vec, gain);
+% text(wc/(2*pi), 0, "\leftarrow center frequency");
+% xlabel('frequency (Hz)');
+% ylabel('gain (dB)');
+% ylim([-100 10])
+% title('Bode Plot of an RLC band pass filter')
+
+% Passive filters - high order pass
+
+r1 = 2.47e3;
+r2 = 2.47e3;
+c1 = 0.033e-6;
+
+freq_vec = 1e2:1:1e6;
 w = freq_vec * 2 * pi;
-wc = r1/L1;
+w0 = 1990*pi;
 
-gain = 20*log10(wc ./ (j*w + wc));
+gain = 20*log10(((-r2/r1) ./(1+j*w*r2*c1)).^3);
 
 figure(1); 
 semilogx(freq_vec, gain);
-text(5e3, -3, "\leftarrow cut off frequency");
+text(w0/(2*pi), -3, "\leftarrow center frequency");
 xlabel('frequency (Hz)');
 ylabel('gain (dB)');
-title('Bode Plot of an RL low pass filter')
+ylim([-100 10])
+title('Bode Plot of an RLC band pass filter')
